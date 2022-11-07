f51654f67f83d883bbaea4b6db128c9261a5f13e
RoopathR <roopathramanathan@gmail.com>
2022-11-07 11:35:46 +0530

    first commit message

---
+++ src/test/java/org/gittest/GithubTest.java
@@ -0,0 +1,24 @@
+package org.gittest;
+
+public class GithubTest {
+
+	public void test1() {
+		System.out.println("1");
+
+	}
+	public void test2() {
+		System.out.println("2");
+
+	}
+	public void test3() {
+		System.out.println("3");
+
+	}
+	public void test4() {
+	System.out.println("4");
+
+	}
+	public void test5() {
+		System.out.println("5");
+	}
+}
